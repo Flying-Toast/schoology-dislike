2152de3873ad27c7c43287c1880669c3d6bfdbf0
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2020-03-27 12:58:37 -0400

    icon

--- extension/dislike.js
+++ extension/dislike.js
@@ -13,6 +13,9 @@ function main() {
 
 		let dislikeButton = document.createElement("span");
 		dislikeButton.className = "dislike-btn";
+		dislikeButton.style.fontWeight = "normal";
+		dislikeButton.style.cursor = "pointer";
+		dislikeButton.style.color = "#4479B3";
 
 		let buttonContent = document.createElement("span");
 		buttonContent.className = "content";
@@ -28,6 +31,14 @@ function loadCustomStyle() {
 	let el = document.createElement("style");
 	document.head.appendChild(el);
 
-	el.sheet.insertRule(`.dislike-btn { font-weight: normal; cursor: pointer; color: #4479B3; }`);
 	el.sheet.insertRule(`.dislike-btn:hover { text-decoration: underline; }`);
+}
+
+function makeDislikeIcon() {
+	let dislikeIcon = document.createElement("img");
+	dislikeIcon.src = browser.extension.getURL("dislike-icon.png");
+	dislikeIcon.width = 16;
+	dislikeIcon.height = 16;
+	dislikeIcon.style.verticalAlign = "text-bottom";
+	return dislikeIcon;
 }
