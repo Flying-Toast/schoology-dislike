32ac18057ee4d33967a44525a56153274faa077b
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2020-03-27 12:32:23 -0400

    style

--- extension/dislike.js
+++ extension/dislike.js
@@ -5,15 +5,14 @@ let loadedCheckInterval = setInterval(function() {
 	}
 }, 500);
 
-
 function main() {
+	loadCustomStyle();
+
 	Array.from(document.querySelectorAll(".like-btn")).forEach(function(i) {
 		const postID = i.id.split("-").pop();
 
 		let dislikeButton = document.createElement("span");
-		dislikeButton.style.fontWeight = "normal";
-		dislikeButton.style.cursor = "pointer";
-		dislikeButton.style.color = "#4479b3";
+		dislikeButton.className = "dislike-btn";
 
 		let buttonContent = document.createElement("span");
 		buttonContent.className = "content";
@@ -23,4 +22,12 @@ function main() {
 		i.parentNode.insertBefore(dislikeButton, i.nextSibling);
 		i.after("· ");
 	});
+}
+
+function loadCustomStyle() {
+	let el = document.createElement("style");
+	document.head.appendChild(el);
+
+	el.sheet.insertRule(`.dislike-btn { font-weight: normal; cursor: pointer; color: #4479B3; }`);
+	el.sheet.insertRule(`.dislike-btn:hover { text-decoration: underline; }`);
 }
